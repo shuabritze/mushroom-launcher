10bf70d3fd1e8d820891d1ec4cff617517645df0
shuabritze <163845127+shuabritze@users.noreply.github.com>
2024-04-03 11:39:15 +0300

    Add Boilerplate (v1.0.0)

---
+++ README.md
@@ -0,0 +1,8 @@
+# Mushroom Launcher
+
+## 🛫 Quick start
+
+1. Clone Repo
+2. Install bun `irm bun.sh/install.ps1 | iex`
+3. Run `bun install`
+4. Run `bun run dev`
---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { Component } from "solid-js";
+
+import logo from "./logo.svg";
+import styles from "./App.module.css";
+
+const App: Component = () => {
+  return (
+    <div class={styles.App}>
+      <header class={styles.header}>
+        <img src={logo} class={styles.logo} alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          class={styles.link}
+          href="https://github.com/solidjs/solid"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Solid
+        </a>
+        <p class="text-lg text-center text-neutral-500 m-4">With Tailwind CSS</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
