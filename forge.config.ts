78ca9f0153535301e51f02692d5f81d03a9888c1
shuabritze <163845127+shuabritze@users.noreply.github.com>
2024-09-21 08:43:26 +0300

    Feat: New version boilerplate

---
+++ forge.config.ts
@@ -0,0 +1,67 @@
+import type { ForgeConfig } from '@electron-forge/shared-types';
+import { MakerSquirrel } from '@electron-forge/maker-squirrel';
+import { MakerZIP } from '@electron-forge/maker-zip';
+import { VitePlugin } from '@electron-forge/plugin-vite';
+import { FusesPlugin } from '@electron-forge/plugin-fuses';
+import { FuseV1Options, FuseVersion } from '@electron/fuses';
+import 'dotenv/config';
+
+const config: ForgeConfig = {
+    packagerConfig: {
+        asar: true,
+    },
+    rebuildConfig: {},
+    makers: [new MakerSquirrel({
+        copyright: 'MIT License Copyright © 2024 shuabritze',
+    }), new MakerZIP({}, ['darwin'])],
+    plugins: [
+        new VitePlugin({
+            // `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
+            // If you are familiar with Vite configuration, it will look really familiar.
+            build: [
+                {
+                    // `entry` is just an alias for `build.lib.entry` in the corresponding file of `config`.
+                    entry: 'src/main.ts',
+                    config: 'vite.main.config.ts',
+                    target: 'main',
+                },
+                {
+                    entry: 'src/preload.ts',
+                    config: 'vite.preload.config.ts',
+                    target: 'preload',
+                },
+            ],
+            renderer: [
+                {
+                    name: 'main_window',
+                    config: 'vite.renderer.config.ts',
+                },
+            ],
+        }),
+        // Fuses are used to enable/disable various Electron functionality
+        // at package time, before code signing the application
+        new FusesPlugin({
+            version: FuseVersion.V1,
+            [FuseV1Options.RunAsNode]: false,
+            [FuseV1Options.EnableCookieEncryption]: true,
+            [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+            [FuseV1Options.EnableNodeCliInspectArguments]: false,
+            [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+            [FuseV1Options.OnlyLoadAppFromAsar]: true,
+        }),
+    ],
+    publishers: [
+        {
+            name: '@electron-forge/publisher-github',
+            config: {
+                repository: {
+                    owner: 'shuabritze',
+                    name: 'mushroom-launcher'
+                },
+                prerelease: true
+            }
+        }
+    ]
+};
+
+export default config;
---
+++ src/main.ts
@@ -0,0 +1,62 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+import 'dotenv/config';
+
+// #region Squirrel Installer
+import ess from 'electron-squirrel-startup';
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (ess) {
+    app.quit();
+}
+// #endregion
+
+// #region Auto Updater
+import { updateElectronApp } from 'update-electron-app';
+updateElectronApp();
+// #endregion
+
+// #region Main Window
+declare const MAIN_WINDOW_VITE_DEV_SERVER_URL: string;
+declare const MAIN_WINDOW_VITE_NAME: string;
+
+const createWindow = () => {
+    // Create the browser window.
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        autoHideMenuBar: true,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+        },
+    });
+
+    mainWindow.removeMenu();
+
+    // and load the index.html of the app.
+    if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
+        mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+    } else {
+        mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
+    }
+
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools();
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
+// #endregion
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
